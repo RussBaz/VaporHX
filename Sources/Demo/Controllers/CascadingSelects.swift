53c662c365df4269cd007bb77db78427fe7cefaf
Brandon <32753167+btoms20@users.noreply.github.com>
2024-03-08 14:54:53 -0800

    Using htmx.prefers and returning the correct content.

--- Sources/Demo/Controllers/CascadingSelects.swift
+++ Sources/Demo/Controllers/CascadingSelects.swift
@@ -5,22 +5,43 @@ struct CascadingSelectController: RouteCollection {
         let select = routes.grouped("select", "models")
         
         select.get { req async throws in
-            if let query = try? req.query.decode(Query.self) {
-                var models:[Model] = []
-                if let make = Make(rawValue: query.make.lowercased()) {
-                    models = make.models
-                }
-                return try await req.htmx.render("CascadingSelect/cascading-select-options", ["models": models])
-            } else {
-                return try await req.htmx.render("CascadingSelect/cascading-select", ["models": Make.Audi.models])
+            // Extract the Make from the URL or default to Audi if no query param is present
+            let make = extractQuery(req: req) ?? .Audi
+            // Switch over the prefered request headers
+            switch req.htmx.prefers {
+            case .html:
+                // The entire page is being requested
+                return try await req.htmx.render("CascadingSelect/cascading-select", ["dto": DTO(make: make.rawValue, models: make.models)])
+            case .htmx:
+                // Just a fragment with the related models is being requested
+                return try await req.htmx.render("CascadingSelect/cascading-select-options", ["dto": DTO(make: make.rawValue, models: make.models)])
+            case .api:
+                // The JSON api is being requested
+                return try! await DTO(make: make.rawValue, models: make.models).encodeResponse(for: req)
             }
         }
     }
     
+    /// Extracts the Make from the url encoded query if one is present
+    private func extractQuery(req:Request) -> Make? {
+        if let query = try? req.query.decode(Query.self) {
+            return Make(rawValue: query.make.lowercased())
+        }
+        return nil
+    }
+    
+    /// A helper struct for sending data into our Leaf template
+    struct DTO:Content {
+        let make:String
+        let models:[Model]
+    }
+    
+    /// The URL encoded query that our HTMX select view generates
     struct Query:Content {
         let make:String
     }
     
+    /// A collection of Makes and Models of cars to choose from
     enum Make:String, Content {
         case Audi   = "audi"
         case Toyota = "toyota"
